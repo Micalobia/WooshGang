8f1f84a54e7011be6f0c165a29622141aecf19a5
Unknown <unknown@example.com>
2020-03-03 17:08:01 -0700

    CubeMove, CameraFollow, CameraChange

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+//smoothSpeed: the laginess of the camera behind the cube
+    // 0 < x < 1
+    // as x approached 1, the closer it follows the cube, until it is directly mimicking
+    //the cube's movements
+
+
+
+
+public class CameraFollow : MonoBehaviour{
+
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+
+    void LateUpdate()
+    {
+
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+    }
+
+
+
+}
---
+++ Assets/CubeMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Notes
+//Always update the default input titles, Unity doesn't recognize the default names
+
+
+
+public class CubeMovement : MonoBehaviour  
+{
+    public float cubemoveSpeed;
+
+    void Start()
+    {
+        cubemoveSpeed = 10;
+    }
+
+    
+    void Update()
+    {
+        transform.Translate(cubemoveSpeed*Input.GetAxis("CubeHorizontalMovement") * Time.deltaTime, 0f, cubemoveSpeed*Input.GetAxis("CubeVerticalMovement") * Time.deltaTime);
+}
+}
